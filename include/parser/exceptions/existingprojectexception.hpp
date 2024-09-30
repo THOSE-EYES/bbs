dcb4389acffb305f6eb1f52eeb1f159aefff7ca9
Illia Shvarov <illya.shvarov@gmail.com>
2024-09-30 09:45:21 +0200

    Add project-related exceptions
    This commit adds 2 new exceptions, which indicate that there is
    a project defined in the file or there are no projects defined
    in the file. Also, unit tests are added for that exceptions.

---
+++ include/parser/exceptions/existingprojectexception.hpp
@@ -0,0 +1,48 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#pragma once
+
+#include <stdexcept>
+#include <string>
+
+namespace parser::exceptions
+{
+/**
+ * @brief An exception, used to notify that the project in the file was already defined
+ * 
+ */
+class ExistingProjectException : public std::runtime_error
+{
+public:
+	/**
+	 * @brief Construct a new ExistingProjectException object
+	 * 
+	 * @param project - the token that caused the error
+	 */
+	explicit ExistingProjectException(std::string name);
+
+protected:
+	/**
+	 * @brief The message, seeing on the exception occurence
+	 * 
+	 */
+	static const std::string kMessage;
+};
+} // namespace parser::exceptions
