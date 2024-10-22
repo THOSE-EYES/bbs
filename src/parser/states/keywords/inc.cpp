fd59b23d730e11cae975e63aad0c392891688ac8
Illia Shvarov <illya.shvarov@gmail.com>
2024-10-22 15:09:42 +0200

    Add !inc keyword
    This commit adds a new !inc keyword, which is used to specify include
    folders.

---
+++ src/parser/states/keywords/inc.cpp
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "parser/states/keywords/inc.hpp"
+
+#include "parser/states/statement.hpp"
+
+namespace parser::states::keywords
+{
+Inc::Inc(Mediator& mediator)
+	: Array{mediator}
+{}
+
+void Inc::Process(lexer::Lexer& lexer)
+{
+	Array::Process(lexer);
+
+	// Set the job's Inc
+	auto& job = mediator_.BorrowJob();
+	for(auto& name : Array::GetValue())
+	{
+		job.AddIncludeDirectory(std::filesystem::path{name});
+	}
+
+	// Return to the Statement state
+	mediator_.SetState(std::make_unique<Statement>(mediator_));
+}
+} // namespace parser::states::keywords
